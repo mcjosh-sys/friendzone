0c5d870fd4d4ff8af943d8fd96cd3ecd36f33236
Olushola Joshua <olusholajoshua7@gmail.com>
2023-12-05 14:39:52 -0600

    second commit

--- test.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://bloximages.newyork1.vip.townnews.com/berkshireeagle.com/content/tncms/assets/v3/editorial/5/85/58505af1-2a89-5558-97b6-aa2dcc84c92a/5fa16e53f2470.image.jpg'
-
-const regex = /(https?:\/\/[^\s]+)/g
-const regex1 = /(https?:\/\/[^\s]+)/g
-const regex2 = /(https?:\/\/[^\s]+)/g
-
-console.log(regex.test(url))
-console.log(regex1.test(url))
-console.log(regex2.test(url))
